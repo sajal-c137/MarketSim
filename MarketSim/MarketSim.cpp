9c85dc612e086c766673fbdb207a0887a63cb7eb
sajal-c137 <sajalagarwal8171@gmail.com>
2026-02-04 17:21:37 +0530

    Initial commit: C++20 CMake project with cppzmq and protobuf

---
+++ MarketSim/MarketSim.cpp
@@ -0,0 +1,20 @@
+﻿// MarketSim.cpp : Defines the entry point for the application.
+//
+
+#include "MarketSim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	
+	// Verify ZeroMQ is working
+	zmq::context_t context(1);
+	cout << "ZeroMQ context created successfully!" << endl;
+	
+	// Verify Protobuf is working
+	cout << "Protobuf version: " << GOOGLE_PROTOBUF_VERSION << endl;
+	
+	return 0;
+}
---
+++ MarketSim/MarketSim.h
@@ -0,0 +1,8 @@
+﻿// MarketSim.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <zmq.hpp>
+#include <google/protobuf/message.h>
